d332845ca71846c94d4d31562bb83664cde0b3a6
Turk <taylankurt34@gmail.com>
2015-05-02 00:16:30 +0300

    In this commit sql file change with right one!

--- src/main/resources/sportClubSystem.sql
+++ src/main/resources/sportClubSystem.sql
@@ -1,8 +1,8 @@
-CREATE DATABASE  IF NOT EXISTS `test` /*!40100 DEFAULT CHARACTER SET utf8 */;
-USE `test`;
+CREATE DATABASE  IF NOT EXISTS `sportclubsystem` /*!40100 DEFAULT CHARACTER SET utf8 */;
+USE `sportclubsystem`;
 -- MySQL dump 10.13  Distrib 5.6.17, for Win64 (x86_64)
 --
--- Host: localhost    Database: test
+-- Host: localhost    Database: sportclubsystem
 -- ------------------------------------------------------
 -- Server version	5.6.21-log
 
@@ -18,141 +18,31 @@ USE `test`;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
--- Table structure for table `category`
+-- Table structure for table `personaltrainers`
 --
 
-DROP TABLE IF EXISTS `category`;
+DROP TABLE IF EXISTS `personaltrainers`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `category` (
-  `CATEGORY_ID` int(10) unsigned NOT NULL AUTO_INCREMENT,
-  `NAME` varchar(10) NOT NULL,
-  `DESC` varchar(255) NOT NULL,
-  PRIMARY KEY (`CATEGORY_ID`) USING BTREE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+CREATE TABLE `personaltrainers` (
+  `idpersonalTrainers` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(45) NOT NULL,
+  `experience` int(11) NOT NULL,
+  `email` varchar(45) NOT NULL,
+  `contact` varchar(45) NOT NULL,
+  PRIMARY KEY (`idpersonalTrainers`),
+  UNIQUE KEY `idpersonalTrainers_UNIQUE` (`idpersonalTrainers`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `category`
+-- Dumping data for table `personaltrainers`
 --
 
-LOCK TABLES `category` WRITE;
-/*!40000 ALTER TABLE `category` DISABLE KEYS */;
-/*!40000 ALTER TABLE `category` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `category_has_stock`
---
-
-DROP TABLE IF EXISTS `category_has_stock`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `category_has_stock` (
-  `category_CATEGORY_ID` int(10) unsigned NOT NULL,
-  `stock_STOCK_ID` int(10) unsigned NOT NULL,
-  PRIMARY KEY (`category_CATEGORY_ID`,`stock_STOCK_ID`),
-  KEY `fk_category_has_stock_stock1_idx` (`stock_STOCK_ID`),
-  KEY `fk_category_has_stock_category1_idx` (`category_CATEGORY_ID`),
-  CONSTRAINT `fk_category_has_stock_category1` FOREIGN KEY (`category_CATEGORY_ID`) REFERENCES `category` (`CATEGORY_ID`) ON DELETE NO ACTION ON UPDATE NO ACTION,
-  CONSTRAINT `fk_category_has_stock_stock1` FOREIGN KEY (`stock_STOCK_ID`) REFERENCES `stock` (`STOCK_ID`) ON DELETE NO ACTION ON UPDATE NO ACTION
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `category_has_stock`
---
-
-LOCK TABLES `category_has_stock` WRITE;
-/*!40000 ALTER TABLE `category_has_stock` DISABLE KEYS */;
-/*!40000 ALTER TABLE `category_has_stock` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `stock`
---
-
-DROP TABLE IF EXISTS `stock`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `stock` (
-  `STOCK_ID` int(10) unsigned NOT NULL AUTO_INCREMENT,
-  `STOCK_CODE` varchar(10) NOT NULL,
-  `STOCK_NAME` varchar(20) NOT NULL,
-  PRIMARY KEY (`STOCK_ID`) USING BTREE,
-  UNIQUE KEY `UNI_STOCK_NAME` (`STOCK_NAME`),
-  UNIQUE KEY `UNI_STOCK_CODE` (`STOCK_CODE`) USING BTREE
-) ENGINE=InnoDB AUTO_INCREMENT=54 DEFAULT CHARSET=utf8;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `stock`
---
-
-LOCK TABLES `stock` WRITE;
-/*!40000 ALTER TABLE `stock` DISABLE KEYS */;
-INSERT INTO `stock` VALUES (50,'1347','Taylan'),(53,'7052','PADINI');
-/*!40000 ALTER TABLE `stock` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `stock_daily_record`
---
-
-DROP TABLE IF EXISTS `stock_daily_record`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `stock_daily_record` (
-  `RECORD_ID` int(10) unsigned NOT NULL AUTO_INCREMENT,
-  `PRICE_OPEN` float(6,2) DEFAULT NULL,
-  `PRICE_CLOSE` float(6,2) DEFAULT NULL,
-  `PRICE_CHANGE` float(6,2) DEFAULT NULL,
-  `VOLUME` bigint(20) unsigned DEFAULT NULL,
-  `DATE` date NOT NULL,
-  `STOCK_ID` int(10) unsigned NOT NULL,
-  PRIMARY KEY (`RECORD_ID`) USING BTREE,
-  UNIQUE KEY `UNI_STOCK_DAILY_DATE` (`DATE`),
-  KEY `FK_STOCK_TRANSACTION_STOCK_ID` (`STOCK_ID`),
-  CONSTRAINT `FK_STOCK_TRANSACTION_STOCK_ID` FOREIGN KEY (`STOCK_ID`) REFERENCES `stock` (`STOCK_ID`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE=InnoDB AUTO_INCREMENT=37 DEFAULT CHARSET=utf8;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `stock_daily_record`
---
-
-LOCK TABLES `stock_daily_record` WRITE;
-/*!40000 ALTER TABLE `stock_daily_record` DISABLE KEYS */;
-INSERT INTO `stock_daily_record` VALUES (36,1.20,1.10,10.00,3000000,'2015-04-27',53);
-/*!40000 ALTER TABLE `stock_daily_record` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `stock_detail`
---
-
-DROP TABLE IF EXISTS `stock_detail`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `stock_detail` (
-  `STOCK_ID` int(10) unsigned NOT NULL AUTO_INCREMENT,
-  `COMP_NAME` varchar(100) NOT NULL,
-  `COMP_DESC` varchar(255) NOT NULL,
-  `REMARK` varchar(255) NOT NULL,
-  `LISTED_DATE` date NOT NULL,
-  PRIMARY KEY (`STOCK_ID`) USING BTREE,
-  CONSTRAINT `FK_STOCK_ID` FOREIGN KEY (`STOCK_ID`) REFERENCES `stock` (`STOCK_ID`)
-) ENGINE=InnoDB AUTO_INCREMENT=51 DEFAULT CHARSET=utf8;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `stock_detail`
---
-
-LOCK TABLES `stock_detail` WRITE;
-/*!40000 ALTER TABLE `stock_detail` DISABLE KEYS */;
-INSERT INTO `stock_detail` VALUES (50,'PADINI Holding Malaysia','one stop shopping','vinci vinci','2015-04-27');
-/*!40000 ALTER TABLE `stock_detail` ENABLE KEYS */;
+LOCK TABLES `personaltrainers` WRITE;
+/*!40000 ALTER TABLE `personaltrainers` DISABLE KEYS */;
+INSERT INTO `personaltrainers` VALUES (1,'Taylan Kurt',110,'taylankurt34@gmail.com','+905384754330'),(2,'Rolandas Janulis',12,'rolandas@gmail.com','+467482943323'),(3,'Viktoras Staisiunas',21,'victoras@gmail.com','+461331235413');
+/*!40000 ALTER TABLE `personaltrainers` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
@@ -164,4 +54,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2015-04-29 12:59:09
+-- Dump completed on 2015-05-02  0:08:55
